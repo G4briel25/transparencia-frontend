a43e0c2421c281abe651319e436e7aef9517ce66
G4briel25 <warmerishoth@gmail.com>
2025-04-13 01:05:39 -0400

    ajuste de link para api produção

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8081",
+    baseURL: "https://api-gerencia.onrender.com",
     headers: {
         "Content-type": "application/json"
     }
